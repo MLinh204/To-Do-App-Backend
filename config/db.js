1f95c5a6e98ff7e874e2ef41a23bd782b88fd1bd
Hoang Manh Linh <linhhoang204@Hoangs-MacBook-Air.local>
2025-03-13 11:04:50 +0700

    Add Docker file

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
+require('dotenv').config({ path: 'security.env' });
 const mysql = require('mysql2/promise');
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'yhZApge06HwfPspi',
-    database: 'todo_app',
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 };
 const pool = mysql.createPool(dbConfig);
 module.exports = pool;
-
-
-
